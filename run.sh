083e303ed4d4c8ef8b29f7cd42ce3bf9c525dfea
HeisenBerg? <edwin_uestc@163.com>
2023-07-07 23:54:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ domain="${URL}"
 
 # Use the input variable value in your script
 echo "The value of 'myInput' is: $domain"
-python emailall.py --domain $domain run > result/$domain.txt
+python emailall.py --domain $domain run   >> result/$domain.txt
